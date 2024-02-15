6f574e1882b7f56f7c69ca39fb104d3812428344
Mo Rahman <mohammed.rahman676@gmail.com>
2024-02-15 15:51:13 +0000

    created array for user input in readme

--- index.js
+++ index.js
@@ -2,3 +2,51 @@ const inquirer = require('inquirer');
 const path = require('path');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown');
+
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project:'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions:'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information:'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'Provide license information:'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution guidelines:'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Provide test instructions:'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }
+];
